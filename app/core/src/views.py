5557a35310bb62faf88e42766b0599af0b0970a2
LuisGoez33 <Nozon33_YT@hotmail.com>
2020-05-27 01:21:14 -0500

    Avances
    Avances en la creacion de la pagina

--- app/core/src/views.py
+++ app/core/src/views.py
@@ -7,8 +7,17 @@ class HomePageView(TemplateView):
 
     template_name = "index.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tituloIni'] = "Texto de titulo"
-        context['titulo2'] = "Otro titulo"
-        return context
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'tituloIni': 'los saluda Luis Pablo Goez', 'titulo2': 'Clases de python'})
+
+
+#class NosotrosPageView(TemplateView):
+
+ #   template_name = "nosotros.html"
+
+    # def get_context_data(self, **kwargs):
+        
+    #     context = super().get_context_data(**kwargs)
+    #     context['tituloIni'] = "Texto de titulo"
+    #     context['titulo2'] = "Otro titulo"
+    #     return context
