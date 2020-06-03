c13ab6c9f3b9700349524fa2436a1ecee5b9f1d3
LuisGoez33 <Nozon33_YT@hotmail.com>
2020-06-03 15:45:05 -0500

    Email listo
    Envio de email de contacto

--- app/core/src/forms.py
+++ app/core/src/forms.py
@@ -8,5 +8,5 @@ class ContactoForm(forms.Form):
     # Atributos del formulario de contacto:
     nombre = forms.CharField(label="Nombre", required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Ingrese su Nombre Completo'}))
     correo = forms.EmailField(label="Correo electrónico", required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Ingrese tu Correo Electronico'}))
-    tipomsj = forms.CharField(label="Tipo de peticion", required=True)
+    tipomsj = forms.ChoiceField(label="Tipo de peticion", required=True, choices=TipoMsj, widget=forms.Select(attrs={'class':'form-control'}))
     descripcion = forms.CharField(label="Descripción", required=True, widget=forms.Textarea(attrs={'class':'form-control', 'rows':'5', 'placeholder':'Escribe tu Mensaje'}))
--- app/core/src/views.py
+++ app/core/src/views.py
@@ -2,8 +2,10 @@ from django.views.generic.base import TemplateView
 from django.shortcuts import render, HttpResponse, redirect
 from django.urls import reverse
 from .forms import ContactoForm
+from django.core.mail import EmailMessage
 
 # Create your views here.
+
 
 class HomePageView(TemplateView):
 
@@ -12,12 +14,14 @@ class HomePageView(TemplateView):
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
 
+
 class QuienesSomosPageView(TemplateView):
 
     template_name = "quienes_somos.html"
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
+
 
 def contacto(request):
     formContacto = ContactoForm()
@@ -27,15 +31,28 @@ def contacto(request):
     if request.method == "POST":
         formContacto = ContactoForm(data=request.POST)
         if formContacto.is_valid():
-            nombre = request.POST.get('Nombre', '')
-            correo = request.POST.get('Correo', '')
-            tipomsj = request.POST.get('TipoMsj', '')
-            descripcion = request.POST.get('Descrip', '')
+            nombre = request.POST.get('nombre', '')
+            correo = request.POST.get('correo', '')
+            tipomsj = request.POST.get('tipomsj', '')
+            descripcion = request.POST.get('descripcion', '')
 
-            # Enviar el mensaje o E-Mail
+            # Creamos el objeto con las variables del formulario:
+            email = EmailMessage(
+                "UpLine: Tienes un nuevo mensaje de contacto",
+                "De {} <{}>\n\nTipo de mensaje:\n{}\n\nEscribió:\n\n{}".format(nombre, correo, tipomsj, descripcion),
+                "no-contestar@inbox.mailtrap.io",
+                ["lpgoez7@misena.edu.co"],
+                reply_to=[correo]
+            )
 
-            return redirect(reverse('contacto')+"?ok")
+            # Enviamos el email
 
+            try:
+                email.send()
+                # Se envio el correo
+                return redirect(reverse('contacto')+"?ok")
+            except:
+                # No se envio el correo
+                return redirect(reverse('contacto')+"?fail")
 
-
-    return render(request, 'contacto.html', {'formulario':formContacto})
+    return render(request, 'contacto.html', {'formulario': formContacto})
