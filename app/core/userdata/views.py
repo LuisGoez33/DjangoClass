cf7c84a680769809829d656c21795e549a9dd092
LuisGoez33 <Nozon33_YT@hotmail.com>
2020-06-19 13:59:49 -0500

    Terminado 2.0

--- app/core/userdata/views.py
+++ app/core/userdata/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.urls import reverse
+from .models import DatosUser
 
 # Create your views here.
 
@@ -12,3 +12,7 @@ class QuienesSomosPageView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
+
+class IndexListView(ListView):
+    model = DatosUser
+    template_name: 'quienes_somos.html'
